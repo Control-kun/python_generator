680004e0194d113b353bec4696bd94eb92c3b206
Pavel <hvostovp@gmail.com>
2019-12-11 23:17:44 +0300

    refactor

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ fake = Faker()
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="123654",
+    password="",
     database="test",
     auth_plugin='mysql_native_password'
 )
@@ -15,10 +15,20 @@ cursor = mydb.cursor()
 
 query = 'INSERT INTO employee(dep_id,name) VALUES (%s, %s)'
 
-for x in range(0, 700):
+for x in range(0, 1000):
     print(x)
     my_data = tuple((random.randint(1, 2), fake.name()) for i in range(0, 1000))
     # print(my_data)
+    cursor.executemany(query, my_data)
+    mydb.commit()
+
+
+query = 'INSERT INTO salary(ts,empl_id,amount) VALUES (%s, %s, %s)'
+
+for x in range(0, 1000):
+    # print(x)
+    my_data = tuple((fake.date_time_this_year(), fake.random_int(1, 1000000), fake.random_int()) for i in range(0, 1000))
+    print(my_data)
     cursor.executemany(query, my_data)
     mydb.commit()
 cursor.close()
